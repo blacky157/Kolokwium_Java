11c850e1ee276a85d2a116f0d8e7265e17f110a7
blacky157 <gruby157@gmail.com>
2021-06-26 18:35:10 +0200

    zaliczenie zd 3

--- src/pl/gda/wsb/Vehicle.java
+++ src/pl/gda/wsb/Vehicle.java
@@ -1,5 +1,23 @@
 package pl.gda.wsb;
 
 public abstract class Vehicle {
+  String registration;
+  String vinNumber;
+  String Kolor;
+  Double Prize;
+  Double fuelConsumption;
+  Double fuelTank;
+  Double mileage;
+
+  public Vehicle(String registration, String vinNumber, String kolor, Double prize, Double fuelConsumption, Double fuelTank, Double mileage) {
+        this.registration = registration;
+        this.vinNumber = vinNumber;
+        Kolor = kolor;
+        Prize = prize;
+        this.fuelConsumption = fuelConsumption;
+        this.fuelTank = fuelTank;
+        this.mileage = mileage;
+    }
+
 
 }
